e99a3b5086e2a03ec4dbdb40c61db706aa86c0cf
bahalacan <81878884+bahalacan@users.noreply.github.com>
2023-01-27 16:57:18 +0300

    Add files via upload

---
+++ ConnectFourGame/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Player player_1 = new Player('X');
+        System.out.println(player_1.getSymbol());
+    }
+}
